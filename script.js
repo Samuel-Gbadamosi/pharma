a9c4a2d6a62e30b84921db79b645e56d0a620778
samuel <damosisamuel@gmail.com>
2023-03-13 10:51:28 +0100

    js + pics

---
+++ script.js
@@ -0,0 +1,18 @@
+// created variable for timeout
+var myTimeOut;
+
+ myTimeOut = setTimeout(myGreeting , 2000);
+
+ myTimeOut2 = setTimeout(mysecondsli , 3000);
+
+ //functions to display timeout on each element in slider
+ function mysecondsli(){
+     document.getElementById('crick').innerHTML = "Explore and feel inspired";
+ }
+
+ function myGreeting(){
+     document.getElementById("greet").innerHTML = "Enjoy your stay on our Website";
+ }
+ function myStopFunction(){
+     clearTimeout(myTimeOut);
+ }
